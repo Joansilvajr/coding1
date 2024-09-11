ca3ed7640b42a027c1e48ff2fbeaf4a3a3319720
João Antonio Jr <joao.asilvajr@outlook.com>
2024-09-11 13:56:08 +0000

    primeirocodigo

---
+++ aula.js
@@ -0,0 +1,8 @@
+console.log("Hello World")
+
+const rotulo = "joao"
+console.log(rotulo)
+
+rotulo = 12345
+console.log(rotulo)
+
---
+++ exemplos.js
@@ -0,0 +1,13 @@
+/*console.log('Hello'+' '+'World')
+
+console.log('Hello'+' '+'World')
+const myName = "Joao"
+console.log("Hello"+' '+ myName)
+myName2 = "Antonio"
+console.log(`Hello ${myName2}`)
+
+var soma = 10 + "25"
+console.log(soma)*/
+
+console.log(15> 10)
+console.log(15> 21)
